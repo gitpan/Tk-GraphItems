7a3a757f5ceae8ece0f93f2fa147167f8f7b7c45
Lamprecht Christoph <lamprecht@cpan.org>
2007-11-26 22:02:44 +0000

    Import of LAMPRECHT/Tk-GraphItems-0.08 from CPAN.
    gitpan-cpan-distribution: Tk-GraphItems
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         LAMPRECHT/Tk-GraphItems-0.08.tar.gz
    gitpan-cpan-author:       LAMPRECHT
    gitpan-cpan-maturity:     released

--- lib/Tk/GraphItems.pm
+++ lib/Tk/GraphItems.pm
@@ -64,22 +64,31 @@ at your option, any later version of Perl 5 you may have available.
 
 =cut
 package Tk::GraphItems;
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 require Tk::GraphItems::TextBox;
 require Tk::GraphItems::Circle;
 require Tk::GraphItems::Connector;
 
 sub TextBox{
-  shift;
-  Tk::GraphItems::TextBox->new(@_);
+    if ($_[0]){
+        shift;
+        return Tk::GraphItems::TextBox->new(@_);
+    }
+    return 'Tk::GraphItems::TextBox';
 }
 
 sub Connector{
-  shift;
-  Tk::GraphItems::Connector->new(@_);
+    if ($_[0]){
+        shift;
+        return Tk::GraphItems::Connector->new(@_);
+    }
+    return 'Tk::GraphItems::Connector';
 }
 sub Circle{
-  shift;
-  Tk::GraphItems::Circle->new(@_);
+    if ($_[0]){
+        shift;
+        return Tk::GraphItems::Circle->new(@_);
+    }
+    return 'Tk::GraphItems::Circle';
 }
 1;
